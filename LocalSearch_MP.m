b7103344d1ac2f3bdf2ea4d82ea1354f953fbbcc
secrowner <secrowner@gmail.com>
2022-11-17 20:29:30 -0700

    update 11/17

--- LocalSearch_MP.m
+++ LocalSearch_MP.m
@@ -10,59 +10,78 @@ syms x1 x2 alpha
 %k = max iterations before termination
 
 %Equation 1:
-%x_0 = [5, 20];
-%vars = [x1, x2];
-%func = 2*x1*x2 + 3*x2 - x1^2 -2*x2^2;
+% vars = [x1, x2];
+% func = 2*x1*x2 + 3*x2 - x1^2 -2*x2^2;
+
 %Equation 2:
-%x_0 = [5, 20];
-%vars = [x1,x2];
-%func = -abs(x1)-abs(x2)-2*sin(x1)-3*sin(x2)+5; 
-%func = -abs(x1)-sin(x1);
-%func = -abs(x2)-2*sin(x1)-1.5*sin(x2);
+% vars = [x1,x2];
+% func = -abs(x1)-abs(x2)-2*sin(x1)-3*sin(x2)+5; 
+% func = -abs(x1)-sin(x1);
+% func = -abs(x2)-2*sin(x1)-1.5*sin(x2);
+
 %Equation 3
 vars = [x1, x2];
 func = -(2*x1-1)*(x1+1)*(x1-2)*(x1+2)-(x2-2)*(x2+2);
 
-k = 10;
+figure(1)
+fsurf(func,[-2 2 -2 2])
+figure(2)
+fcontour(func,[-10 10 -10 10])
+
+k = 3;
 
 %% Multiple Start Point Steepest Ascent
-x_set = randi([-1000,1000], 5, 2);
+num_start_points = 5;
+x_set = randi([-100,100], num_start_points, 2);
+f_initial_list = zeros(num_start_points, 1);
+for i=1:length(x_set(:,1))
+    f_initial_list(i,:) = subs(func, vars, x_set(i,:));
+end
+x_final_list = zeros(num_start_points, 2);
+f_final_list = zeros(num_start_points, 1);
 grad = gradient(func,vars);
-i = 0;
+
 tic
 for j = 1:length(x_set(:,1))
+    j
     x_final = x_set(j,:);
     x_prev = x_final+100;
-    while abs(norm(x_final-x_prev)) > 0.001 & i < k
+    i = 0;
+    while abs(norm(x_final-x_prev)) > 0.001 && i < k
         %find alpha equation
         alpha_eq = x_final' + alpha*subs(grad, vars, x_final);
-        alpha_opt = solve(diff(simplify(subs(simplify(func),vars,alpha_eq')))==0, alpha, 'PrincipalValue', true); 
-        
+        alpha_opt = solve(diff(simplify(subs(simplify(func),vars,alpha_eq')))==0, alpha, 'PrincipalValue', true);
+
         x_prev = x_final;
         x = x_final' + alpha_opt*subs(grad, vars, x_final);
         x_final = double(x');
         i = i + 1;
     end
-    if j == 1
-        x_last_best = x_final;
-    end
-    if j~=1 & double(subs(func, vars, x_final)) > double(subs(func, vars, x_last_best))
-        x_last_best = x_final;
-    end
+    x_final_list(j,:) = x_final;
+    f_final_list(j,:) = subs(func, vars, x_final);
 end
+[best_value, best_index] = max(f_final_list)
+x_best = x_final_list(best_index, :)
 toc
-double(double(subs(func, vars, x_last_best)))
-double(x_last_best)
-%% Multiple Start Point Newton's Method
 
-x_set = randi([-1000,1000], 5, 2);
+%% Multiple Start Point Newton's Method
+num_start_points = 5;
+x_set = randi([-100,100], num_start_points, 2);
+f_initial_list = zeros(num_start_points, 1);
+for i=1:length(x_set(:,1))
+    f_initial_list(i,:) = subs(func, vars, x_set(i,:));
+end
+x_final_list = zeros(num_start_points, 2);
+f_final_list = zeros(num_start_points, 1);
 grad = gradient(func,vars);
 H = hessian(func, vars);
-i = 0;
+
 tic
 for j = 1:length(x_set(:,1))
+    j
     x_final = x_set(j,:);
     x_prev = x_final+100;
+    i = 0;
     while abs(norm(x_final-x_prev)) > 0.001 & i < k
         %find alpha equation
         alpha_eq = x_final' + alpha*subs(inv(H),vars,x_final)*subs(grad, vars, x_final);
@@ -73,13 +92,9 @@ for j = 1:length(x_set(:,1))
         x_final = double(x');
         i = i + 1;
     end
-    if j == 1
-        x_last_best = x_final;
-    end
-    if j~=1 & double(subs(func, vars, x_final)) > double(subs(func, vars, x_last_best))
-        x_last_best = x_final;
-    end
+    x_final_list(j,:) = x_final;
+    f_final_list(j,:) = subs(func, vars, x_final);
 end
+[best_value, best_index] = max(f_final_list)
+x_best = x_final_list(best_index, :)
 toc
-double(double(subs(func, vars, x_last_best)))
-double(x_last_best)
--- LocalSearch_SA.m
+++ LocalSearch_SA.m
@@ -1,5 +1,7 @@
-%Ryan Reschak
+%Ryan Reschak, Scott Crowner
 clear
+clc
+
 syms x1 x2 alpha
 
 %x_0 = starting point
@@ -9,26 +11,33 @@ syms x1 x2 alpha
 
 
 %Equation 1:
-%x_0 = [5, 20];
-%vars = [x1, x2];
-%func = 2*x1*x2 + 3*x2 - x1^2 -2*x2^2;
-%Equation 2:
-%x_0 = [5, 20];
-vars = [x1,x2];
-%func = -abs(x1)-abs(x2)-2*sin(x1)-3*sin(x2)+5; 
-%func = -abs(x1)-sin(x1);
-%func = -abs(x2)-2*sin(x1)-1.5*sin(x2);
-%Equation 3
-x_0 = [-100, -100];
-vars = [x1, x2];
-func = -(2*x1-1)*(x1+1)*(x1-2)*(x1+2)-(x2-2)*(x2+2);
+% x_0 = [5, 20];
+% vars = [x1, x2];
+% func = 2*x1*x2 + 3*x2 - x1^2 -2*x2^2;
 
-k = 20;
+%Equation 2:
+x_0 = [5, 20];
+vars = [x1,x2];
+func = -abs(x1)-abs(x2)-2*sin(x1)-3*sin(x2)+5;
+% func = -abs(x1)-sin(x1);
+% func = -abs(x2)-2*sin(x1)-1.5*sin(x2);
+
+%Equation 3
+% x_0 = [-100, -100];
+% vars = [x1, x2];
+% func = -(2*x1-1)*(x1+1)*(x1-2)*(x1+2)-(x2-2)*(x2+2);
+
+figure(1)
+fsurf(func,[-10 10 -10 10])
+figure(2)
+fcontour(func,[-10 10 -10 10])
+
+k = 10;
 annel = 0.1;
 annel_iters = 4;
 %% Steepest Ascent
 x_final = x_0;
-grad = gradient(func,vars)
+grad = gradient(func,vars);
 i = 0;
 t = 0.9;
 x_prev = x_0+100;
@@ -55,13 +64,13 @@ while double(norm(x_final-x_prev)) > 0.001 & i < k
             x_final = x';
 
         end
-        
         double(x_final)
         y_old = subs(func, vars, x_final);
         i = i + 1;
     end
     t = t/(t*annel+1); 
 end
+double(subs(func, vars, x_final))
 toc
 %% Newtons method
 x_final = x_0;
@@ -101,4 +110,5 @@ while abs(norm(x_final-x_prev)) > 0.001 & i < k
     end
     t = t/(t*annel+1); 
 end
+double(subs(func, vars, x_final))
 toc
